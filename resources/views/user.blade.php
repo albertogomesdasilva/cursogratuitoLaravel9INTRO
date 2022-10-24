a3ddf4922ef707a64a18c7540fcaafcdc0ea18db
Alberto Gomes da Silva <albertogomesdasilva@hotmail.com>
2022-10-24 16:27:04 -0300

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,6 @@
+<h1>Passando variável direto: {{ $name }} </h1>
+<hr>
+<h1>Listando o Retorno da Model User</h1>
+<h2>{{ $user }} </h2>
+<p>Selecionando o nome fica: {{ $user->name }} </p>
+<p>Selecionando o Email fica: {{ $user->email }} </p>
